bcc100f431bd6257f2604723c59bedd0f7718965
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2024-09-22 15:42:00 -0400

    chore: added a bar chart for analysis

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+import pandas as pd
 import numpy as np
 
 st.set_page_config(page_title="Dashboard", page_icon="📈")
@@ -7,7 +8,11 @@ st.set_page_config(page_title="Dashboard", page_icon="📈")
 st.markdown("# Dashboards")
 
 st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
+    """This demo illustrates a combination of plotting and analysis that have been done in Observables and others as part of mimilabs.ai powerful dataset in Databricks. Enjoy!"""
 )
+
+st.markdown("## Most Prescribed Drug from 2020 - 2024")
+new_data = pd.read_csv("/Users/gisseldiaz/Desktop/Gi Projects/gi_chatbot/most_prescribed_drug_20to24_2024_05_16.csv")
+
+
+st.bar_chart(new_data, x="gnrc_name", y="total_claims", x_label="Generic Drug Name", y_label="Total Claims per Drug")
